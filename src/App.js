ab9a772f5916ebbd1d3e4ce843e66ef4f213124b
jhleeO <jhlee@olulo.io>
2021-05-17 09:32:18 +0900

    validator

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import logo from './logo.svg';
 import React, { Component, useState } from 'react'; 
 import './App.css';
 
-const useInput = initialValue => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = event => {
-    console.log(event.target);
-  }
+    const {
+      target: {value}
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
   return {value, onChange};
-}
+};
 
 
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLen = value => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <div cvlassName = "App">
         <h1>Hello</h1>
         <input placeholder="Name" {...name} />
     </div>
   );
-}
+};
 
 export default App;
